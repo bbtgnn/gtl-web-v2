80d3157d44ed54372726dfe98b1d96ad03b66299
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-03-21 19:37:19 +0100

    load pyodide from url

--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -1,9 +1,12 @@
-import { loadPyodide } from 'pyodide';
+// @ts-expect-error Url imports are not typed
+import { loadPyodide } from 'https://cdn.jsdelivr.net/pyodide/v0.27.4/full/pyodide.mjs';
+import type { loadPyodide as LoadPyodide } from 'pyodide';
+
 import * as Comlink from 'comlink';
 
 //
 
-const pyodideReadyPromise = loadPyodide();
+const pyodideReadyPromise = (loadPyodide as typeof LoadPyodide)();
 
 const api = {
 	runPython: async (python: string, context: Record<string, unknown>) => {
@@ -11,6 +14,7 @@ const api = {
 		const pyodide = await pyodideReadyPromise;
 		// Now load any packages we need, run the code, and send the result back.
 		await pyodide.loadPackagesFromImports(python);
+		await pyodide.loadPackage('micropip');
 		// make a Python dictionary with the data from `context`
 		const dict = pyodide.globals.get('dict');
 		const globals = dict(Object.entries(context));
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, type PluginOption } from 'vite';
-
-//
-
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const PYODIDE_EXCLUDE = ['!**/*.{md,html}', '!**/*.d.ts', '!**/*.whl', '!**/node_modules'];
-
-export function viteStaticCopyPyodide(): PluginOption {
-	const pyodideDir = dirname(fileURLToPath(import.meta.resolve('pyodide')));
-	return viteStaticCopy({
-		targets: [
-			{
-				src: [join(pyodideDir, '*')].concat(PYODIDE_EXCLUDE),
-				dest: '_app/immutable/workers'
-			}
-		]
-	});
-}
+import { defineConfig } from 'vite';
 
 //
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), viteStaticCopyPyodide()],
-
-	optimizeDeps: {
-		exclude: ['pyodide']
-	},
-
-	worker: {
-		format: 'es'
-	},
+	plugins: [tailwindcss(), sveltekit()],
 
 	test: {
 		workspace: [
